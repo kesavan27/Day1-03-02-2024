adeeef0ad47bfdd6d07cc06240581f54a4de1e14
kesavan27 <gvkesavan2003@gmail.com>
2024-02-04 10:34:04 +0530

    task day1

---
+++ q2.js
@@ -0,0 +1,5 @@
+// Write a blog about objects and its internal representation in Javascript
+
+let car = {name: 'maruthi', model: 'wagnr',year: 2021,cc: 1200, warranty: 'five years',owner: 'G Vedanthadesikan'};
+
+console.log(car.name, car.model, car.year, car.cc, car. warranty, car.owner);
